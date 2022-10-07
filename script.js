026b12886f98d8d2f3ac717b11e15c2f07d4a2e8
Brody Moorman <brodymoorman@outlook.com>
2022-10-06 23:07:05 -0400

    Added input functionallity plus round win loss logic, also added a function that runs 5 rounds declaring "win", "loss", or "tie". However, doenst keep score.

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-let computerChoice = getComputerChoice();
-        console.log(computerChoice)
-        
+
+    game();
+
         
     function getComputerChoice(){
         const rand = Math.floor(Math.random() * 3);
         if(rand === 0){
-            return "Rock";
+            return "rock";
         }else if(rand === 1){
-            return "Paper";
+            return "paper";
         }else if(rand === 2){
-            return "Scisors";
+            return "scissors";
+        }
+    }
+    function getUserChoice(){
+        let input = prompt("Enter Choice: ")
+        let temp = input.toLowerCase();
+        return temp;
+    }
+    function playRound(playerChoice, computerChoice){
+        if(playerChoice === computerChoice ){
+            console.log("Its a tie")
+        }
+        if((playerChoice==="rock" && computerChoice === "scissors")||(playerChoice==="paper" && computerChoice === "rock")||(playerChoice==="scissors" && computerChoice === "paper")){
+            console.log("You Win!")
+        }
+        if((computerChoice==="rock" && playerChoice === "scissors")||(computerChoice==="paper" && playerChoice === "rock")||(computerChoice==="scissors" && playerChoice === "paper")){
+            console.log("You lose!")
+        }
+    }
+    function game(){
+        let playerScore = 0;
+        let compterScore = 0;
+        for (let i = 0; i < 5; i++){
+            let computerChoice = getComputerChoice();
+            let userChoice = getUserChoice();
+    
+            playRound(userChoice, computerChoice);
         }
     }
